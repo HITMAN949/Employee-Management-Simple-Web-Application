b15c66303df9a67317b7c5bdc222fb235ca3f959
Lasfar Houssam <74057656+HITMAN949@users.noreply.github.com>
2025-01-21 11:45:42 +0100

    Update login.php

--- views/login.php
+++ views/login.php
@@ -3,9 +3,8 @@ session_start();
 
 
 $validUsers = [
-    ['email' => 'sanaa.el.mansouri@ocpgroup.ma', 'password' => 'sanaa.el.mansouri123', 'name' => 'Sanaa El Mansouri'],
-    ['email' => 'adil.moussaoui@ocpgroup.ma', 'password' => 'adil.moussaouid123', 'name' => 'Adil Moussaoui'],
-    ['email' => 'nora.el.khattabi@ocpgroup.ma', 'password' => 'nora.el.khattabi123', 'name' => 'Nora El Khattabi']
+    ['email' => 'username@email.com', 'password' => 'Password', 'name' => 'username']
+    
 ];
 
 
